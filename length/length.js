19fe814b3044f26bf5b8e98ca5721348364598f3
sumanareddy <sumanareddy.guc27@gmail.com>
2020-03-09 09:06:21 -0500

    last commit

---
+++ length/length.js
@@ -0,0 +1,18 @@
+var word_Length = (x) => {
+    console.log(x);
+    return x.length }
+  
+  var word_Length = new Vue({
+      el: '#word_Length',
+      data: {
+        word : 'rakeshkumar'
+        },
+      computed: {
+          length : function () {
+            const i = this.word
+            return `The Length of a given name is ${word_Length(i)}.`
+              
+          }
+        }
+      })
+    
